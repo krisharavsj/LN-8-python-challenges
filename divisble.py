e7159a5f4c717cb4bb7026f206af46a325ce1752
krisharavsj <krisharsv@gmail.com>
2025-06-03 09:42:47 +0530

    divisble or not.py

---
+++ divisble.py
@@ -0,0 +1,6 @@
+numb = int(input("please enter the numerator"))
+number=int(input("please enter you denominator"))
+if numb%number==0:
+    print("yes your number is divisible")
+else:
+    print("no your number is not divisible")
